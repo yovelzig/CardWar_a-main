c81188076b0842c98cf672f464c6ee5c189fa546
yovelzig <yovel20031@gmail.com>
2023-03-28 20:55:43 +0300

    final

--- Test.cpp
+++ Test.cpp
@@ -3,7 +3,7 @@
 #include "sources/game.hpp"
 #include "sources/card.hpp"
 #include <stdexcept>
-using namespace std;
+// using namespace std;
 using namespace ariel;
 TEST_CASE("initialization") {
     Player p1("Alice");
@@ -41,28 +41,33 @@ TEST_CASE("changes in each stack after the game") {
     CHECK(p1.cardesTaken() + p2.cardesTaken() == 52);
     CHECK(p2.stacksize() + p2.stacksize() == 0);
 }
-
-
-TEST_CASE("check for errors") {
-    ariel::Player p1("Alice");
-    ariel::Player p2("Bob");     
-    ariel::Game game(p1,p2);
-    CHECK_THROWS(game.printLastTurn());
-    CHECK_THROWS(game.printLog());
-    game.playTurn();
-    CHECK_THROWS(game.printWiner());
-    
-    // game.printStats()
-    CHECK(p1.stacksize() < 26);
-    CHECK(p2.stacksize() < 26);
-    CHECK(p1.cardesTaken() + p2.cardesTaken() > 0);
-    CHECK((p1.cardesTaken() + p1.stacksize() + p2.cardesTaken() + p2.stacksize()) == 52);
-    CHECK(p2.stacksize() + p2.stacksize() < 52);
+TEST_CASE("check for notThrow error") {
+    Player p1("Alice");
+    Player p2("Bob");     
+    Game game(p1,p2);
+    game.playAll();
+    CHECK_NOTHROW(game.printLastTurn());
+    CHECK_NOTHROW(game.printLog());
+    CHECK_NOTHROW(game.printStats());
+    CHECK_NOTHROW(game.printWiner());
 } 
 
-
-// TEST_CASE("Factorials of negative numbers") { 
-//     CHECK_THROWS(factorial(-1));  // check that some exception is thrown
-//     CHECK_THROWS_AS(factorial(-2), std::out_of_range);  // check that a specific exception type is thrown
-//     CHECK_THROWS_AS(factorial(-2), std::exception);  // check that a specific exception type (or a descendant) is thrown
-//}
+TEST_CASE("check for errors") {
+    Player p1("Alice");
+    Player p2("Bob");     
+    Game game(p1,p2);
+    CHECK_THROWS(game.printLastTurn());
+    CHECK_THROWS(game.printLog());
+    CHECK_THROWS(game.printWiner()); 
+    CHECK_THROWS(game.printStats()); 
+} 
+TEST_CASE("check for error if we init only single Player") {
+    Player p1("Alice");    
+    Game game(p1,p1);
+    CHECK_THROWS(game.playTurn());
+    CHECK_THROWS(game.playAll());
+    CHECK_THROWS(game.printLog());
+    CHECK_THROWS(game.printWiner()); 
+    CHECK_THROWS(game.printStats()); 
+    CHECK_THROWS(game.printLastTurn()); 
+} 
